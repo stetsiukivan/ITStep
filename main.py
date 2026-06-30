46336fcbe05a2aa7f4199296e187868d92862450
stetsiukivan <stetsiuk.ivan26@google.com>
2026-06-30 18:40:26 +0200

    math_functions.py imported into main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello Git!")
+
+from math_functions import rectangle_area
+
+side_a = 8
+side_b = 5
+
+area = rectangle_area(side_a, side_b)
+
+print(f"Площадь прямоугольника со сторонами {side_a} и {side_b} равна: {area}")
