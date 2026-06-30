e6a6d465bfc42d23a805a96fd12dcb0118576db0
stetsiukivan <stetsiuk.ivan26@google.com>
2026-06-30 18:57:38 +0200

    imported rectangle_perimeter in main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 print("Hello Git!")
 
-from math_functions import rectangle_area
+from math_functions import rectangle_area, rectangle_perimeter
 
 side_a = 8
 side_b = 5
 
 area = rectangle_area(side_a, side_b)
+perimeter = rectangle_perimeter(side_a, side_b)
 
 print(f"Площадь прямоугольника со сторонами {side_a} и {side_b} равна: {area}")
+print(f"Периметр прямоугольника со сторонами {side_a} и {side_b} равен: {perimeter}")
